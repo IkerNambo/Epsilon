13f768bdf3b9b2e8ae3bcbaa0741ca59166a4be5
ikernambo <ikernamboproject@gmail.com>
2024-12-23 21:57:15 -0600

    added the change password and change email functionality.. and other qol stuff i forgot

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm, UserChangeForm
 from django.contrib.auth import get_user_model 
+from django.core.exceptions import ValidationError
+
 
 CustomUserUserUser = get_user_model()
 
@@ -18,6 +20,7 @@ class UserForm(UserCreationForm):
             'username': None,
             'email': None,
         }
+   
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username',
                                                                 'class': 'form-control',
                                                                 }))
@@ -32,8 +35,51 @@ class UserForm(UserCreationForm):
                                                                 'class': 'form-control',
                                                                 'label': 'password'
                                                                 }))
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Email already exists')
+        return email
     
 class ChangePasswordForm(PasswordChangeForm):
     pass
 
-
+class ChangeEmailForm(UserChangeForm):
+    password = None
+    username = None
+    error_class = 'error-control'
+    
+    
+    class Meta:
+        model = User
+        fields = ['email']
+        
+        
+    email = forms.CharField(label='Enter new email', widget=forms.EmailInput(attrs={'placeholder': 'Email',
+                                                                'class': 'form-control-global',
+                                                                }))
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Email is already in use ')
+        return email
+    
+class ChangePasswordForm(PasswordChangeForm):
+    
+    old_password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={
+        'placeholder': 'old password',
+        'class': 'form-control-global password-control',
+    }))
+    new_password1 = forms.CharField(label='New Password',required=True, widget=forms.PasswordInput(attrs={
+        'placeholder': 'New password',
+        'class': 'form-control-global password-control',
+    }))
+    new_password2 = forms.CharField(label='Confirm New Password',required=True, widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm New password',
+        'class': 'form-control-global password-control',
+    }))
+    
+    
+    
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
-from .forms import UserForm
+from .forms import UserForm, ChangeEmailForm, ChangePasswordForm
 import time
 from django.contrib.auth import authenticate, login, logout
 from .mail import RegisterMail
@@ -24,7 +24,7 @@ def RegisterView(request,*args,**kwargs):
       else:
           UserForm() 
     except ValueError:
-        registermessage = messages.success(request, "registration failed, check the information and try again")
+        registermessage = messages.success(request, "Registration failed, check the information and try again")
     authuser = request.user.is_authenticated
 
   
@@ -38,10 +38,10 @@ def RegisterView(request,*args,**kwargs):
 def LoginView(request, *ruby, **rose):
     if request.method == 'POST':
         username = request.POST.get('username')
-        email = request.POST.get('email')
+        
         password = request.POST.get('password')
         
-        user = authenticate(username=username,email=email, password=password)
+        user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('EmptyView')
@@ -66,32 +66,80 @@ def UserManageView(request, *blake, **belladona):
         return redirect('EmptyView')
 
 def UsernameChangeView(request, *args, **kwargs):
+    
     if request.user.is_authenticated == True:
+        if request.method == 'POST':
+            newusername = request.POST.get('username')
+            print(newusername)
+            user = request.user
+            userlength = len(newusername)
+            
+            if newusername != '' and newusername is not None and userlength <= 20:
+              user.username = newusername  
+              user.save()
+              
+              return redirect('Settings')
+            else:
+                userms = messages.success(request, 'Please enter a valid name')
       
-      
-      
-      
-      return render(request, 'configuration/usernamereset.html', {})
+        return render(request, 'configuration/usernamereset.html', {})
     else:
         return redirect('EmptyView')
 
 def EmailChangeView(request, *args, **kwargs):
     if request.user.is_authenticated == True:
+        form = ChangeEmailForm()
+        
+        if request.method == 'POST':
+          form = ChangeEmailForm(request.POST, instance=request.user)
+          
+          if form.is_valid():
+              form.save()
+              return redirect('Settings')
+            
+            
+        
+        # if request.method == 'POST':
+        #     form = request.POST.get('emailchangeform')
+        #     newemail = request.POST.get('email')
+        #     confirmemail = request.POST.get('email2')
+          
+        #     user = request.user
+        #     userlength = len(newemail)
+            
+        #     if newemail != '' and newemail is not None and userlength <= 50 and newemail == confirmemail:
+        #       user.email = newemail
+        #       user.save()
+              
+        #       return redirect('Settings')
+        #     elif newemail != confirmemail:
+        #         messages.error(request, 'The emails do not match')
+        #     else:
+        #         messages.error(request, 'Enter a valid email')
       
       
       
-      
-      return render(request, 'configuration/emailreset.html', {})
+        context = {
+            'form': form
+        }
+        return render(request, 'configuration/emailreset.html', context)
     else:
       return redirect('EmptyView')
 
 def PasswordChangeView(request, *args, **kwargs):
     if request.user.is_authenticated == True:
-      
-      
-      
-      
-      return render(request, 'configuration/passwordchange.html', {})
+        form = ChangePasswordForm(User)
+        
+        if request.method == 'POST':
+            form = ChangePasswordForm(request.user, request.POST)
+            if form.is_valid():
+                form.save()
+        
+        context = {
+            'form': form
+        }
+                
+        return render(request, 'configuration/passwordchange.html', context)
     else:
       return redirect('EmptyView')
 
